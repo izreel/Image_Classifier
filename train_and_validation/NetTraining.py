ff118ff5976bed89ad3ace3238ac078b3415444c
Isrrael Perez <israpere@gmail.com>
2019-10-10 13:20:27 -0500

    modifying training

--- train_and_validation/NetTraining.py
+++ train_and_validation/NetTraining.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import train_and_validation.NetValidation
+import numpy as np
 
 #Trains a model under a dataset
-def train(model, training_set, validation_set, optimizer, loss_funct, epochs= 30):
+def train(model, training_set, validation_set, optimizer, criterion, epochs= 30):
     #model.cuda()
-
+    valid_loss_min = np.Inf
     for i in range(epochs):
         train_loss = 0
         validation_loss = 0
@@ -19,13 +21,24 @@ def train(model, training_set, validation_set, optimizer, loss_funct, epochs= 30
 
             output = model(data)
 
-            loss = loss_funct(output, label)
+            loss = criterion(output, label)
             loss.backward()
 
             optimizer.step()
 
-            train_loss += loss.item()*data.size(0)
+            train_loss += loss.item()
 
         train_loss /= len(training_set.sampler)
 
-        print("Epoch: {} \tTraining loss: {:.6f}".format(i+1, train_loss))
+        print("Epoch: {} \tTraining loss: {:.6f}".format(i+1, train_loss), end= '')
+        
+        model.eval()
+        with torch.no_grad():
+            validation_loss, accuracy = train_and_validation.NetValidation.validate(model, validation_set, criterion )
+        
+        print("\tTest loss: {:.6f} \tCurrent accuracy: {}".format(validation_loss, accuracy))
+
+        if validation_loss <= valid_loss_min:
+            print("Saving weights")
+            torch.save(model.state_dict(), "model_weights.pt")
+            valid_loss_min = validation_loss
