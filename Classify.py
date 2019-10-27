7b7dd14a9019bb765438cbe7b66a512dca84e239
Isrrael Perez <israpere@gmail.com>
2019-10-27 01:52:14 -0500

    testing model using another dataset

--- Classify.py
+++ Classify.py
@@ -5,6 +5,22 @@ import train_and_validation.NetTraining
 import train_and_validation.NetAccuracy
 import torch.nn as nn
 import torch.optim as optim
+import cv2
+import os
+
+def image_input(image):
+
+    image = cv2.imread(image)
+    image = cv2.resize(image, (32,32))
+
+    cv2.imshow("animal", image)
+    cv2.waitKey(0)
+    
+    image = torch.tensor(image, dtype= torch.float)
+    image = image.view((1,3, 32, 32))
+
+    
+    return image
 
 
 #class labels in CIFAR-10 dataset
@@ -21,11 +37,22 @@ classes = ['airplane',
 
 model = ObjectNet.ObjectNet()
 
-# model.load_state_dict(torch.load("model_weights.pt"))
+model.load_state_dict(torch.load("model_weights_1.pt"))
 model.cuda()
 
 print("Setting up training set, validation set and test set using CIFAR10 dataset")
 train_set, validation_set, test_set = train_and_validation.DatasetLoader.get_dataset('data')
 print("Starting to train model")
-train_and_validation.NetTraining.train(model, train_set, validation_set, optim.SGD(model.parameters(), lr= 0.01), nn.CrossEntropyLoss(), 60)
+# train_and_validation.NetTraining.train(model, train_set, validation_set, optim.SGD(model.parameters(), lr= 0.01), nn.CrossEntropyLoss(), 35)
 print(train_and_validation.NetAccuracy.accuracy(model, test_set)*100)
+
+accuracy  = 0
+for i in range(12500):
+    print(i, end= '\t')
+    if os.path.exists('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)):
+        output = model(image_input('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)).cuda())
+        print(output)
+        print(classes[output.topk(1, dim=1).indices[0]])
+        if classes[output.topk(1, dim=1).indices[0]] == 'dog':
+            accuracy += 1
+print(accuracy/12500 * 100)
--- train_and_validation/NetAccuracy.py
+++ train_and_validation/NetAccuracy.py
@@ -1,11 +1,11 @@
 import numpy as np
 import torch
+
 def accuracy(model, test_set):
     acc = 0
 
     for images, labels in test_set:
         images, labels = images.cuda(), labels.cuda()
-
         output = (model(images))
 
         for i in range(images.shape[0]):
