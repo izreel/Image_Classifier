0786db2770903b992ce2b1d8a679cb5732ace6cd
Isrrael Perez <israpere@gmail.com>
2019-10-28 01:12:00 -0500

    Testing out another model

--- Classify.py
+++ Classify.py
@@ -1,4 +1,5 @@
 import torch
+import model.AnimalNet as AnimalNet
 import model.ObjectNet as ObjectNet
 import train_and_validation.DatasetLoader
 import train_and_validation.NetTraining
@@ -35,24 +36,26 @@ classes = ['airplane',
         'ship',
         'truck']
 
-model = ObjectNet.ObjectNet()
+train_model = True
+# model = ObjectNet.ObjectNet()
+model = AnimalNet.AnimalNet()
 
-model.load_state_dict(torch.load("model_weights_1.pt"))
+# model.load_state_dict(torch.load("model_weights_1.pt"))
 model.cuda()
+if train_model:
+    print("Setting up training set, validation set and test set using CIFAR10 dataset")
+    train_set, validation_set, test_set = train_and_validation.DatasetLoader.get_dataset('data')
+    print("Starting to train model")
+    train_and_validation.NetTraining.train(model, train_set, validation_set, optim.SGD(model.parameters(), lr= 0.01), nn.CrossEntropyLoss(), 50)
+    print(train_and_validation.NetAccuracy.accuracy(model, test_set))
 
-print("Setting up training set, validation set and test set using CIFAR10 dataset")
-train_set, validation_set, test_set = train_and_validation.DatasetLoader.get_dataset('data')
-print("Starting to train model")
-# train_and_validation.NetTraining.train(model, train_set, validation_set, optim.SGD(model.parameters(), lr= 0.01), nn.CrossEntropyLoss(), 35)
-print(train_and_validation.NetAccuracy.accuracy(model, test_set)*100)
-
-accuracy  = 0
-for i in range(12500):
-    print(i, end= '\t')
-    if os.path.exists('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)):
-        output = model(image_input('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)).cuda())
-        print(output)
-        print(classes[output.topk(1, dim=1).indices[0]])
-        if classes[output.topk(1, dim=1).indices[0]] == 'dog':
-            accuracy += 1
-print(accuracy/12500 * 100)
+# accuracy  = 0
+# for i in range(12500):
+#     print(i, end= '\t')
+#     if os.path.exists('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)):
+#         output = model(image_input('Cat_Dog_data/train/dog/dog.{}.jpg'.format(i)).cuda())
+#         print(output)
+#         print(classes[output.topk(1, dim=1).indices[0]])
+#         if classes[output.topk(1, dim=1).indices[0]] == 'dog':
+#             accuracy += 1
+# print(accuracy/12500 * 100)
--- model/AnimalNet.py
+++ model/AnimalNet.py
@@ -1,15 +1,16 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-#TODO: Research other classification CNN architectures that can be implemented for this task
-
-class ObjectNet(nn.Module):
+class AnimalNet(nn.Module):
     def __init__(self):
-        super(ObjectNet, self).__init__()
+        super(AnimalNet, self).__init__()
 
         self.conv1 = nn.Conv2d(3, 16, 3, padding= 1)
         self.conv2 = nn.Conv2d(16, 32, 3, padding= 1)
         self.conv3 = nn.Conv2d(32, 64, 3, padding= 1)
+        self.conv32 = nn.Conv2d(64, 64, 3, padding= 1)
+        self.conv22 = nn.Conv2d(32, 32, 3, padding= 1)
+        self.conv12 = nn.Conv2d(16, 16, 3, padding= 1)
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(p= 0.2)
         self.fc1 = nn.Linear(4*4*64, 512)
@@ -17,9 +18,12 @@ class ObjectNet(nn.Module):
         self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv12(x)))
+        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv22(x)))
+        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv32(x)))
 
         #Flattens image in order to go through the fully connnected layers
         x = x.view(-1, 4*4*64)
